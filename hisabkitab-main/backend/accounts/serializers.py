e437fc0fa87bbe2bd8b1518d5bb7e9453408874a
piyushsketch <piyush72504@gmail.com>
2026-08-16 13:01:16 +0530

    update

--- hisabkitab-main/backend/accounts/serializers.py
+++ hisabkitab-main/backend/accounts/serializers.py
@@ -1,12 +1,17 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
+import logging
 
 User = get_user_model()
+
+logger = logging.getLogger(__name__)
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'email', 'shop_name', 'owner_name', 'phone']
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -16,12 +21,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         fields = ['email', 'password', 'shop_name', 'owner_name', 'phone']
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['email'], # Use email as username
-            email=validated_data['email'],
-            password=validated_data['password'],
-            shop_name=validated_data['shop_name'],
-            owner_name=validated_data['owner_name'],
-            phone=validated_data['phone']
-        )
-        return user
+        try:
+            user = User.objects.create_user(
+                username=validated_data['email'],
+                email=validated_data['email'],
+                password=validated_data['password'],
+                shop_name=validated_data['shop_name'],
+                owner_name=validated_data['owner_name'],
+                phone=validated_data['phone']
+            )
+            return user
+
+        except Exception:
+            logger.exception("SIGNUP ERROR")
+            raise
